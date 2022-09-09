c2e93de70a9925e37176f64369d9e9002f466edc
Charles <charlieford477@gmail.com>
2022-09-09 08:09:02 -0700

    adding 5-printf.c to repo

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *Discription: main - this program prints text
+ *
+ *Return: always return 0 if successful
+ */
+
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  return (0);
+}
