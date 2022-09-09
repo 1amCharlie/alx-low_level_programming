aa46634402f582a69b3d00d0bfa6766bc85c4a3e
Charles <charlieford477@gmail.com>
2022-09-09 08:57:13 -0700

    amend 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,10 +16,10 @@ long int c;
 long long int d;
 float e;
 
-printf("Size of char: %lu byte(s)\n", sizeof(a));
-printf("Size of int: %lu byte(s)\n", sizeof(b));
-printf("Size of long int: %lu byte(s)\n", sizeof(c));
-printf("Size of long long int: %lu byte(s)\n", sizeof(d));
-printf("Size of float: %lu byte(s)\n", sizeof(e));
+printf("Size of char: %ld byte(s)\n", sizeof(a));
+printf("Size of int: %ld byte(s)\n", sizeof(b));
+printf("Size of long int: %ld byte(s)\n", sizeof(c));
+printf("Size of long long int: %ld byte(s)\n", sizeof(d));
+printf("Size of float: %ld byte(s)\n", sizeof(e));
 return (0);
 }
