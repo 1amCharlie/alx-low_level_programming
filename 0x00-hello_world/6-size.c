b68fa95eca93f256b17fe9eb319831022f414293
Charles <charlieford477@gmail.com>
2022-09-09 08:46:41 -0700

    amend 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * Description: main - printing datatype sizes
+ *Description: main - printing datatype sizes
  *
  *Resturn: 0 for success
  */
@@ -19,5 +19,5 @@ printf("Size of int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("Size of float: %lu byte(s)\n", (unsigned long)sizeof(e));
- return (0);
+return (0);
 }
