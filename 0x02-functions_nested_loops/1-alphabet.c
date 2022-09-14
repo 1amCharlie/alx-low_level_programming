04ed7538a2d91f30302cec889d848199138dc075
Charles <charlieford477@gmail.com>
2022-09-14 12:41:20 -0700

    adding 1-alphabet.c to repo

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *print_alphabet - print the alphabet in lowercase letters
+ *Return: void
+ */
+
+void print_alphabet(void)
+{
+int ii;
+
+for (ii = 'a'; ii <= 'z'; ii++)
+_putchar(ii);
+_putchar('\n');
+}
