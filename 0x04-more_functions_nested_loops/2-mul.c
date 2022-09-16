e38140fcc2f65f6a74e2406b8d8e4e6ccf9c1312
Charles <charlieford477@gmail.com>
2022-09-16 10:32:31 -0700

    adding 2-mul.c main.h to repo

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - function that multiplies two integers
+ * @num1: first variable
+ * @num2: second variable
+ *
+ * Return: num1 * num2
+ */
+
+int mul(int num1, int num2)
+{
+return (num1 * num2);
+}
