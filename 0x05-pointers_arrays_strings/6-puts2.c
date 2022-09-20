eba14bb0be5f4d98374633bddf8527b08a29608d
Charles <charlieford477@gmail.com>
2022-09-20 12:02:09 -0700

    adding 6-puts2.c to repo

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#tdio.h>
+#include "main.h"
+
+/**
+ * puts2 - prints one char out of 2 of a string
+ * @str: string to print the chars from
+ * Return: Always 0
+ */
+void puts2(char *str)
+{
+int i = 0;
+while (i >= 0)
+{
+if (str[i] == '\0')
+{
+putchar('\n');
+break;
+}
+if (i % 2 == 0)
+putchar(str[i]);
+i++;
+}
+}
