a69e262616e767785b89a216eb07ccb4ad268805
Charles <charlieford477@gmail.com>
2022-09-21 11:54:00 -0700

    amend char *_strcat(char *dest, char *src)

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,22 +9,20 @@
 
 char *_strcat(char *dest, char *src)
 {
-
 int i = 0;
-int n = 0;
+int j = 0;
 
 while (dest[i] != '\0')
+i++;
+
+while (src[j] != '\0')
 {
+*(dest + i) = src[j];
+j++;
 i++;
 }
 
-while (src[n] != '\0')
-{
-*(dest + i) = src[n];
-n++;
-i++;
-}
-*(dest + 1) = '\0';
+*(dest + i) = '\0';
 
 return (dest);
 }
